4d83cde153ce6d00e3dbe75e6210e871ec01295f
Darkmift <Darkmift@users.noreply.github.com>
2021-03-30 20:21:09 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const result = require('dotenv').config();
+if (result.error) {
+    throw result.error;
+}
+
+const express = require('express');
+const cors = require('cors');
+const config = require('./config');
+const errorHandler = require('./middleware/errorHandler.middleware');
+
+const app = express();
+
+const corsOptions = {
+    origin: config.origin,
+    credentials: true,
+};
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, 'public')));
+} else {
+    app.use(cors(corsOptions));
+}
+
+const mainRouter = require('./main.routes');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello' });
+});
+
+app.use('/api', mainRouter);
+
+// global error handler
+app.use(errorHandler);
+process
+    .on('unhandledRejection', (reason, p) => {
+        Logger.error(reason, 'Unhandled Rejection at Promise', p);
+    })
+    .on('uncaughtException', (err) => {
+        Logger.error(err, 'Uncaught Exception thrown');
+        process.exit(1);
+    });
+
+const PORT = process.env.PORT || 3030;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+    console.log('NODE_ENV', process.env.NODE_ENV);
+});
---
+++ services/mongo.service.js
@@ -0,0 +1,37 @@
+const Logger = require('./logger.service');
+const ObjectId = require('mongodb').ObjectId;
+
+var dbConn = null;
+var prmConn = null;
+const url = process.env.MONGO_URI;
+let dbName = url.substring(config.dbURL.lastIndexOf('/') + 1);
+
+function connectToMongo() {
+    if (prmConn) return prmConn;
+    // Reuse existing connection if exist
+    if (dbConn) return Promise.resolve(dbConn);
+
+    const MongoClient = require('mongodb').MongoClient;
+
+    prmConn = MongoClient.connect(url, { useNewUrlParser: true }).then(
+        (client) => {
+            Logger.info('[MONGO SERVICE] new connection to db');
+            prmConn = null;
+            // If we get disconnected (e.g. db is down)
+            client.on('close', () => {
+                Logger.info('[MONGO SERVICE] MongoDB Diconnected!');
+                dbConn = null;
+                client.close();
+            });
+            dbConn = client.db();
+            return dbConn;
+        }
+    );
+    return prmConn;
+}
+
+module.exports = {
+    connect: connectToMongo,
+    ObjectId,
+    dbName,
+};
